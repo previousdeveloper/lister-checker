f71338d3ce678617fa9ba08a3feebff313d9267c
previousdeveloper <gokhan.karadas1992@gmail.com>
2015-09-20 16:43:17 +0300

    init.

---
+++ test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by previousdeveloper on 20.09.2015.
+ */
+var test = require("./lib/index");
+
+
+test.check('/Volumes/Untitled/Github/Lister/file.txt', function (data) {
+
+    console.log(data);
+});
